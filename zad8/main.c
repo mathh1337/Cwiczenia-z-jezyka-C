d28eb821082f13bf47667d678b28e498e526bba5
mathh1337 <maaatekk@gmail.com>
2022-12-22 14:52:51 +0100

    Zadanie 8  - NWD dwóch liczb

--- zad8/main.c
+++ zad8/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int NWD(int x, int y){
+int NWD(int x, int y){ //funkcja rozwiazujaca NWD rekurencyjnie
     if(y!=0) {
         return NWD(y, x % y);
-
     }
     return x;
 }
@@ -13,21 +12,19 @@ int main() {
     while(1) {
         // Wczytywanie zmiennej x
         printf("\nWprowadz x: ");
-        while (scanf("%d", &x) != 1) { // sprawdzamy, czy użytkownik podał poprawną liczbę
+        while (scanf("%d", &x) != 1 || x <= 0 || x != (int)x) { // sprawdzamy, czy użytkownik podał poprawną liczbę
             printf("To nie jest poprawna liczba. Wprowadz x: ");
-            while (getchar() != '\n'); // usuwa niepoprawne dane z bufora wejściowego
+            while (getchar() != '\n');
         }
 
         // Wczytywanie zmiennej y
         printf("Wprowadz y: ");
-        while (scanf("%d", &y) != 1) { // sprawdzamy, czy użytkownik podał poprawną liczbę
+        while (scanf("%d", &y) != 1 || y <= 0 || y != (int)y) { // sprawdzamy, czy użytkownik podał poprawną liczbę
             printf("To nie jest poprawna liczba. Wprowadz y: ");
-            while (getchar() != '\n'); // usuwa niepoprawne dane z bufora wejściowego
+            while (getchar() != '\n');
         }
 
-        printf("NWD dla liczb %d i %d wynosi: ", x, y);
-        NWD(x, y);
-        printf("%d", NWD(x, y));
+        printf("NWD dla liczb %d i %d wynosi: %d", x, y, NWD(x, y));
     }
     return 0;
 }
